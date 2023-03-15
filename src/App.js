67a55d49653086abf60a4dce78935364cf8e2846
Saul Florez <sflorez@globaltask.net>
2023-03-15 16:18:59 -0400

    📦 NEW: GlobalStyles y lista de categorias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
-  <Category />
+  <div>
+    <GlobalStyles />
+    <ListOfCategories />
+  </div>
 )
